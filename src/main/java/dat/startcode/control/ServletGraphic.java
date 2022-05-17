205f09bfd0f0b98e35dc71955b5afaafe7a63092
Mark Lundgaard <marklundgaard7@gmail.com>
2022-05-17 11:03:46 +0200

    svg

--- src/main/java/dat/startcode/control/ServletGraphic.java
+++ src/main/java/dat/startcode/control/ServletGraphic.java
@@ -2,15 +2,21 @@ package dat.startcode.control;
 
 import com.mysql.cj.Session;
 import dat.startcode.model.config.ApplicationStart;
+import dat.startcode.model.entities.Order;
+import dat.startcode.model.entities.Product;
 import dat.startcode.model.entities.User;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.persistence.ProductMapper;
 import dat.startcode.model.persistence.UserMapper;
+import dat.startcode.model.services.Calculator;
+import dat.startcode.model.services.SVG;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "ServletGraphic", value = "/ServletGraphic")
 public class ServletGraphic extends HttpServlet {
@@ -28,8 +34,34 @@ public class ServletGraphic extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String carportWidth = request.getParameter("carportWidth");
-        String carportLength = request.getParameter("carportLength");
+        // Opret ordre
+        int carportWidth = Integer.parseInt(request.getParameter("carportWidth"));
+        int carportLength = Integer.parseInt(request.getParameter("carportLength"));
+        Order order = new Order((User) request.getSession().getAttribute("user"), carportWidth, carportLength);
+
+
+        ProductMapper productMapper = new ProductMapper(connectionPool);
+        ArrayList<Product> products = null;
+        try {
+            products = productMapper.retrieveProduct();
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+
+        Calculator calc = new Calculator(order,products);
+
+        int rafterQuantity = order.getPartsListLines().get(5).getQuantity();
+
+        SVG svg = new SVG(0, 0, "0 0 800 600", 100, 50);
+
+        svg.addRect(0, 15, 4.5, carportLength);
+        svg.addRect(0, carportWidth - 15, 4.5, carportLength);
+
+
+        for (int x = 0; x < rafterQuantity; x++) {
+            svg.addRect(100 + 50 * x, 0, carportWidth, 4.5);
+        }
+        request.setAttribute("svgdrawing", svg.toString());
         String address = request.getParameter("address");
         if(address != null){
             UserMapper userMapper = new UserMapper(connectionPool);
