925008aed76b620ae64c032b3ecaffd454f5b639
Mark Lundgaard <marklundgaard7@gmail.com>
2022-05-18 10:24:39 +0200

    SVG

--- src/main/java/dat/startcode/control/ServletGraphic.java
+++ src/main/java/dat/startcode/control/ServletGraphic.java
@@ -7,6 +7,7 @@ import dat.startcode.model.entities.Product;
 import dat.startcode.model.entities.User;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.persistence.OrderMapper;
 import dat.startcode.model.persistence.ProductMapper;
 import dat.startcode.model.persistence.UserMapper;
 import dat.startcode.model.services.Calculator;
@@ -40,7 +41,6 @@ public class ServletGraphic extends HttpServlet {
         int carportLength = Integer.parseInt(request.getParameter("carportLength"));
         Order order = new Order((User) request.getSession().getAttribute("user"), carportWidth, carportLength);
 
-
         ProductMapper productMapper = new ProductMapper(connectionPool);
         ArrayList<Product> products = null;
         try {
@@ -49,32 +49,61 @@ public class ServletGraphic extends HttpServlet {
             e.printStackTrace();
         }
 
-        Calculator calc = new Calculator(order,products);
+        Calculator calc = new Calculator(order, products);
         calc.calcPartsList();
 
         int rafterQuantity = order.getPartsListLines().get(5).getQuantity();
+        int postQuantity = order.getPartsListLines().get(6).getQuantity();
 
-        SVG svg = new SVG(0, 0, "0 0 800 600", 600, 600);
 
+        SVG svg = new SVG(100, 50, "0 0 800 600", 600, 600);
+
+        SVG arrowSvg = new SVG(carportLength,carportWidth, "0 0 800 600", 600, 600);
+
+
+        // Alle spær
         svg.addRect(0, 15, 4, carportLength);
         svg.addRect(0, carportWidth - 15, 4, carportLength);
-
-
         for (int x = 0; x < rafterQuantity; x++) {
-            svg.addRect(2 + carportLength/(rafterQuantity-1) * x, 0, carportWidth, 4);
+            svg.addRect(2 + carportLength / (rafterQuantity - 1) * x, 0, carportWidth, 4);
         }
-        request.setAttribute("svgdrawing", svg.toString());
+
+        // stolper
+        for (int y = 0; y < 2; y++) {
+
+            // Stolper i hver hjørne
+            for (int x = 0; x < 2; x++) {
+                svg.addRect(2 + (carportLength - 12) * x, (carportWidth - 30) * y + 12, 12, 12);
+            }
+
+            // Stolper i midten
+            if (postQuantity > 4) {
+                svg.addRect(carportLength / 2, (carportWidth - 30) * y + 12, 12, 12);
+            }
+        }
+
+        // Stålbånd på kryds
+        svg.addLine(2 + carportLength / (rafterQuantity - 1), 12, 2 + carportLength-(carportLength / (rafterQuantity - 1)), carportWidth - 12);
+        svg.addLine(2 + carportLength-(carportLength / (rafterQuantity - 1)), 12, 2 + carportLength / (rafterQuantity - 1), carportWidth - 12);
+
+        // Arrows
+        arrowSvg.addSvg(svg);
+        arrowSvg.addArrow(carportLength, carportWidth);
+
+
+        request.setAttribute("svgdrawing", arrowSvg.toString());
         String address = request.getParameter("address");
         request.setAttribute("order", order);
-        if(address != null){
+        if (address != null) {
             UserMapper userMapper = new UserMapper(connectionPool);
             try {
                 User userSession = (User) request.getSession().getAttribute("user");
-                if(userSession != null){
-                    if(!address.equals(userSession.getAddress())){
+                if (userSession != null) {
+                    if (!address.equals(userSession.getAddress())) {
                         userMapper.updateAddress(userSession.getEmail(), address);
                         User user = new User(userSession.getEmail(), userSession.getFullName(), userSession.getBalance(), address, userSession.getZipNr(), userSession.getRole());
                         request.getSession().setAttribute("user", user);
+
                     }
                 }
             } catch (DatabaseException e) {
